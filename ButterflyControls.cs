611a20c4f5f907f95691bd79c26fd09f2c32c3b4
Cameron Partee <cp3082@nyu.edu>
2019-04-19 13:23:14 -0400

    Add files via upload

---
+++ ButterflyControls.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyControls : MonoBehaviour {
+
+    private Animation myAnimation;
+    private bool shouldMove = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        myAnimation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(shouldMove) {
+            transform.Translate(Vector3.forward * Time.deltaTime * (transform.localScale.x * .05f));
+        }
+    }
+
+
+    public void Walk() {
+
+        if(!myAnimation.isPlaying) {
+            myAnimation.Play();
+            shouldMove = true;
+        } else {
+            myAnimation.Stop();
+            shouldMove = false;
+        }
+    }
+
+    public void LookAt() {
+        transform.LookAt(Camera.main.transform.position);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+
+    public void Bigger() {
+        transform.localScale += new Vector3(1, 1, 1);
+    }
+
+    public void Smaller() {
+        if(transform.localScale.x > 1)  {
+            transform.localScale -= new Vector3(1, 1, 1);
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
